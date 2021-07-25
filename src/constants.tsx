950ec7b4e89c060f70e157ee2c0eeab434560ec3
ThatRand0mUser <rand.user.42069@protonmail.com>
2021-07-25 18:56:04 +0100

    new contract

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,5 +2,5 @@ export default {
     CHAIN_ID: "holodeck-2",
     SSCRT_CONTRACT_ADDRESS: "secret1s7c6xp9wltthk5r6mmavql4xld5me3g37guhsx",
     SEFI_CONTRACT_ADDRESS: "secret12q2c5s5we5zn9pq43l0rlsygtql6646my0sqfm",
-    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1q0yvr9q3u5kzauxv0ntdeyuk2v4hqx7jc800x8",
+    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1stxflaqrdsve34ezh23z9ndawjg6hk9ul88dls",
 } 
