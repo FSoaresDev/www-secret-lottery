d66cd28d00bd528e02aad9483722010267600e85
ThatRand0mUser <rand.user.42069@protonmail.com>
2021-08-30 12:22:27 +0100

    update secret lottery contract address

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,5 +2,5 @@ export default {
     CHAIN_ID: "holodeck-2",
     SSCRT_CONTRACT_ADDRESS: "secret1s7c6xp9wltthk5r6mmavql4xld5me3g37guhsx",
     SEFI_CONTRACT_ADDRESS: "secret12q2c5s5we5zn9pq43l0rlsygtql6646my0sqfm",
-    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1dk2fh42qzwp2yds5vzhx9pkk988d7w2zjs29gv",
+    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret165e0mkzqltwvpve0mxuc73duq7psewzvl626uc",
 } 
