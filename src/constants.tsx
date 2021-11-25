0aab5cf45641457faf071e755ef9051cefcac1d9
ThatRand0mUser <rand.user.42069@protonmail.com>
2021-11-25 13:52:12 +0000

    new test contracts

--- src/constants.tsx
+++ src/constants.tsx
@@ -3,6 +3,8 @@ export default {
     RPC: "http://185.107.80.166:26667",
     REST: "http://185.107.80.166:1317",
     SSCRT_CONTRACT_ADDRESS: "secret1s7c6xp9wltthk5r6mmavql4xld5me3g37guhsx",
-    SEFI_CONTRACT_ADDRESS: "secret1ks94mga6dfunh88wj887jq7fz97unwg2dxgt4y",
-    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1mlp734x5nf2hkamq807x3yvq88vre2l4j92crc",
+    SEFI_CONTRACT_ADDRESS: "secret10xy2dz4df5rrqsjf8wjreh6ejrqwt6y7a4gunn",
+    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1tp2y9t4cu7xvcklrgv58yypqsjj9ru2p6utmdk",
 }
+
+
