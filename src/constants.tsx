3d46dbc76623937faf344b96658949a85acbcd27
ThatRand0mUser <rand.user.42069@protonmail.com>
2021-12-06 09:39:05 +0000

    new contract testnet

--- src/constants.tsx
+++ src/constants.tsx
@@ -4,7 +4,7 @@ export default {
     REST: "http://185.107.80.166:1317",
     SSCRT_CONTRACT_ADDRESS: "secret1s7c6xp9wltthk5r6mmavql4xld5me3g37guhsx",
     SEFI_CONTRACT_ADDRESS: "secret10xy2dz4df5rrqsjf8wjreh6ejrqwt6y7a4gunn",
-    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1tp2y9t4cu7xvcklrgv58yypqsjj9ru2p6utmdk",
+    SECRET_LOTTERY_CONTRACT_ADDRESS: "secret1mfg3kxl9kj5ful2m6wt2qqq2wyypnw4s7t2cd8",
 }
 
 
